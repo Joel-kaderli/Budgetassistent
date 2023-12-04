197eb7b093292612499345734c440135451075ad
Joel-kaderli <142605851+Joel-kaderli@users.noreply.github.com>
2023-12-04 14:40:37 +0100

    Add files via upload

---
+++ Budgetplaner/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Budgetplaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Miete",
+            "ÖV Kosten",
+            "Essen",
+            "Abos",
+            "Auto Kosten",
+            "ander Fix Kosten"});
+            this.comboBox1.Location = new System.Drawing.Point(867, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(867, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Speichern";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(867, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 383);
+            this.chart1.Name = "chart1";
+            series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Enabled = false;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1088, 161);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 12);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(435, 347);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 544);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Budgetplaner/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Budgetplaner
+{
+    public partial class Form1 : Form
+    {
+        private string[] auswahlArray;
+        private Chart chart;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Annahme: Die ComboBox ist bereits mit Elementen gefüllt
+            auswahlArray = new string[comboBox1.Items.Count];
+
+            // Erstelle das Chart-Objekt und fügen Sie es zum Formular hinzu
+            chart = new Chart();
+            chart.Dock = DockStyle.Fill;
+
+            // Füge dem Diagramm eine Diagrammfläche hinzu
+            ChartArea chartArea = new ChartArea();
+            chart.ChartAreas.Add(chartArea);
+
+            // Erstelle eine neue Datenreihe
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Column;
+
+            // Füge die Daten aus der Datei zur Datenreihe hinzu
+            UpdateChart();
+
+            // Füge die Datenreihe zum Diagramm hinzu
+            chart.Series.Add(series);
+
+            // Fügen Sie das Diagrammsteuerelement zum Formular hinzu
+            Controls.Add(chart);
+
+            // sicher stellen, dass das Diagramm sichtbar ist
+            chart.Visible = true;
+
+        }
+        private const string FileName = "ausgaben.txt";
+        private void UpdateChartAutomatically()
+        {
+            // Aktualisiert das Diagramm automatisch, wenn Daten hinzugefügt werden
+            UpdateChart();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+     
+        private void AddDataToSeries(Series series, string data)
+        {
+            if (data != null)
+            {
+                string[] parts = data.Split(' ');
+                if (parts.Length == 2)
+                {
+                    string auswahl = parts[0];
+                    decimal betrag;
+                    if (decimal.TryParse(parts[1], out betrag))
+                    {
+                        series.Points.AddXY(auswahl, betrag);
+                    }
+                }
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Überprüfen  ob eine Auswahl getroffen wurde
+            if (comboBox1.SelectedIndex != -1)
+            {
+                // Überprüfen  ob ein Betrag eingegeben wurde
+                if (decimal.TryParse(textBox1.Text, out decimal betrag))
+                {
+                    // Speichern der Auswahl und den Betrag in der Datei
+                    string auswahl = comboBox1.SelectedItem.ToString();
+                    int selectedIndex = comboBox1.SelectedIndex;
+
+                    if (selectedIndex >= 0 && selectedIndex < auswahlArray.Length)
+                    {
+                        auswahlArray[selectedIndex] = $"{auswahl} {betrag:C}";
+
+                        // Speichern des Array in der Datei
+                        SaveArrayToFile();
+
+                        MessageBox.Show($"Auswahl und Betrag in Datei gespeichert: {auswahl}, {betrag:C}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ungültiger Index für die Auswahl.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bitte einen gültigen Betrag eingeben.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte eine Auswahl treffen.");
+            }
+        }
+
+        private void SaveArrayToFile()
+        {
+            string dateipfad = "ausgaben.txt";
+
+            try
+            {
+                File.WriteAllLines(dateipfad, auswahlArray);
+                MessageBox.Show("Array erfolgreich in Datei gespeichert.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Speichern des Arrays: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Speichern des  Array in der Datei, bevor das Formular geschlossen wird
+            SaveArrayToFile();
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            // Aktualisieren das Diagramm, wenn der Button geklickt wird
+            UpdateChart();
+        }
+
+        private void UpdateChart()
+        {
+            if (chart.Series.Count > 0)
+            {
+                // Löschen der vorhandene Daten aus der Datenreihe
+                chart.Series[0].Points.Clear();
+
+                // F Daten aus dem Array zur Datenreihe hinzu
+                foreach (string data in auswahlArray)
+                {
+                    AddDataToSeries(chart.Series[0], data);
+                }
+            }
+            chart.Visible = true;
+        }
+        
+    }
+}
+
